2abe64c1977aba688df337197373fd0949855e23
pavan-bommideni <pavan-bommideni@users.noreply.github.com>
2019-07-18 18:06:55 +0530

    Update EmployeeController.java

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,9 +2,15 @@ package com.example.demo.controller;
 
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@RestController("/api")
 public class EmployeeController {
+	
+	@GetMapping("/test")
+	public String getMessage(){
+	return "Hello";	
+	}
 	
 
 

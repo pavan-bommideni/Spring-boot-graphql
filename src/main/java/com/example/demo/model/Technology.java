76081f7b61fec9931e6f1369d2b68a7cd09e7e2d
pavan-bommideni <bpkpavan0208@gmail.com>
2019-07-30 04:40:05 +0530

    Graphql with REST API

---
+++ src/main/java/com/example/demo/model/Technology.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+
+import java.util.List;
+
+
+public class Technology {
+                      
+    public String id;
+
+    public String technologyName;
+    public List<String> skillset;
+    private Integer userId;
+    
+    public Technology() {}
+
+	public Technology(String id, String technologyName, List<String> skillset) {
+		super();
+		this.id = id;
+		this.technologyName = technologyName;
+		this.skillset = skillset;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTechnologyName() {
+		return technologyName;
+	}
+
+	public void setTechnologyName(String technologyName) {
+		this.technologyName = technologyName;
+	}
+
+	public List<String> getSkillset() {
+		return skillset;
+	}
+
+	public void setSkillset(List<String> skillset) {
+		this.skillset = skillset;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	
+}
--- src/main/java/com/example/demo/resolver/QueryResolver.java
+++ src/main/java/com/example/demo/resolver/QueryResolver.java
@@ -2,14 +2,25 @@ package com.example.demo.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.example.demo.entity.Employee;
+import com.example.demo.model.Technology;
 import com.example.demo.respository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class QueryResolver implements GraphQLQueryResolver {
+	
+	@Value("${technolgies-service}")
+	String technologiesApi;
 	
 	public QueryResolver() {
 		System.out.println("QueryResolver Intialised");
@@ -46,6 +57,21 @@ public class QueryResolver implements GraphQLQueryResolver {
     public List<Employee> getEmployeesByPaging(Integer start,Integer end){
     	System.out.println("getEmployeesByPaging Invoked for Starting " + start + " Ending "+ end);
     	return employeeRepository.findEmployeesByRange(start-1, end);
+    }
+    
+    public List<Technology> getTechnologies(){
+    	System.out.println("getTechnologies API "+ technologiesApi);
+    	RestTemplate restTemplate = new RestTemplate();
+    	ResponseEntity<List<Technology>> response = 
+    	restTemplate.exchange(technologiesApi,
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Technology>>() {
+        });
+    	if(response.getStatusCode().equals(HttpStatus.OK)) {
+    		return response.getBody();
+    	}
+    	else {
+        	return new ArrayList<Technology>();
+    	}
     }
 
 }
